090826c3a2dc7422fb036760358cde980d943bb0
Fusted <tihantsov@gmail.com>
2021-11-18 23:38:18 +0300

    burger

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import './scss/reset.scss'
 import './scss/typography.scss'
 import './scss/style.scss'
 import './scss/section.scss'
+import './scss/roadmap.scss'
+import './scss/burger.scss'
+
 import './scss/adaptive.scss'
 
 
 
-import './js/test'
+import './js/script';
+
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.nav__toggle')
+const ul = document.querySelector('.nav__links')
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('btn--active')
+    ul.classList.toggle('ul--active')
+})
