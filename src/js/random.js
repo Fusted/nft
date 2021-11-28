3907c61b8b1a072b58e3d418721a8e871daae57a
Fusted <74307000+Fusted@users.noreply.github.com>
2021-11-28 14:31:31 +0300

    Delete random.js

--- src/js/random.js
+++
@@ -1,9 +0,0 @@
-export default function randomWord(){
-    const abc = "abcdefghijklmnopqrstuvwxyz"
-    let rs = ''
-
-    while (rs.length < 8) {
-        rs += abc[Math.floor(Math.random() * abc.length)];
-    }
-    return rs
-}
