ea2c24c4aa1807514aef7264a59948b41020dd49
Fusted <74307000+Fusted@users.noreply.github.com>
2021-11-28 14:32:13 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-import {qr2} from './pages/qr2'
-
 window.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('.nav__toggle')
     const ul = document.querySelector('.nav__links')
@@ -24,21 +22,19 @@ window.addEventListener('DOMContentLoaded', () => {
     // roadmap
 
     const roadmap = document.querySelector('.roadmap')
-    var word = ''
+    // const textList = document.querySelectorAll('.roadmap__info')
 
     roadmap.addEventListener('click', (e) => {
-
         if (e.target.classList.contains('btn--box')){
             const num = parseInt(e.target.getAttribute('data-btn'))
             const active = roadmap.querySelectorAll('.roadmap__info--active')
             roadmap.querySelector('.box--active').classList.remove('box--active')
             e.target.classList.add('box--active')
 
-
-            if (num == 6 && localStorage.getItem('hfynakgyv') == 'true'){
-                roadmap.addEventListener('click', (e) => pas(e))
-                console.error('UGFzc3dvcmQ6IDAxMDEgMDAxMSAxMDAxIDAwMDE') //5391
-            }
+            // for (let i = 1; i < num + 1; i++){
+            //     roadmap.querySelector(`[data-btn='${i}'`).classList.add('box-checked')
+            //     roadmap.querySelector(`[data-btn='${i}'`).previousSibling.classList.add('box--line-checked')
+            // }
 
             if (active.length){
                 active.forEach(item => {
@@ -48,36 +44,6 @@ window.addEventListener('DOMContentLoaded', () => {
     
             (roadmap.querySelector(`[data-info='${num}'`)).classList.add('roadmap__info--active')
         }
-        
     })
 
-
-    function pas(e){
-        if (e.target.classList.contains('btn--box')){
-            word += parseInt(e.target.getAttribute('data-btn'))
-            if (word.length > 4){
-                word = word.slice(1, 5)
-            }
-
-            if (word == '5391'){
-                const a = prompt('drowssap')
-                if (a == '1935'){
-                    if (confirm('JS это безопасно?')){
-                        if (confirm('Уверен?')){
-                            alert('Земля тебе металлом')
-                        } else {
-                            alert('Не зря сомневаешься')
-                        }
-                    } else {
-                        alert ('aHR0cDovL3FyY29kZXIucnUvY29kZS8/aHR0cHMlM0ElMkYlMkZ5b3V0dS5iZSUyRmRRdzR3OVdnWGNRJTNGdCUzRDg1JjQmMA==')   
-                    }
-                    
-                } else if (a == '5391'){
-                    alert('Это все???')
-                } else {
-                    alert('Не угадал')
-                }
-            }
-        } 
-    }
 })
